877350f45e5d0a27c93afba953957250f1d50463
Trong Hoang <hoangngotrong2004@gmail.com>
2025-06-10 09:28:33 +0700

    data

--- src/route/web.js
+++ src/route/web.js
@@ -6,15 +6,7 @@ let initWebRoutes = (app) => {
     // Render the home page
     router.get('/', homeController.getHomePage);
 
-    // Render the about page
-    router.get('/about', (req, res) => {
-        return res.render('about.ejs');
-    });
 
-    // Render the contact page
-    router.get('/contact', (req, res) => {
-        return res.render('contact.ejs');
-    });
 
     // Use the router in the app
     app.use('/', router);
